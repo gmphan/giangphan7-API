12db77df1901cc16b2c0503be8f2205f7f3fc9f5
Giang Phan <giangphan@Giangs-MacBook-Pro.local>
2017-11-02 15:58:38 -0400

    done with reminder

--- routes/reminders/index.js
+++ routes/reminders/index.js
@@ -13,10 +13,29 @@ function handleReminder(req, reply){
   })
 }
 /***------ handleReminders --------****/
+
+/******* handleInsertReminder **********/
+function handleInsertReminder(req, reply){
+  (async function(){
+    const {reminder_name, reminder_quote, written_by} = req.payload;
+    var results=await db.execute(sql.reminder.insert, [reminder_name, reminder_quote, written_by]);
+    reply();
+  })()
+  .catch((err)=>{
+    throw err;
+  })
+}
+/***---- end handleInsertReminder---***/
+
 module.exports=[
   {
     method:'GET',
     path:'/reminder',
     handler:handleReminder
+  },
+  {
+    method:'POST',
+    path:'/insert/reminder',
+    handler:handleInsertReminder
   }
 ]
