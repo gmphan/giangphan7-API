62473e2321cc136fcab840de4278a98346a92216
Giang Phan <giangphan@Giangs-MacBook-Pro.local>
2017-12-02 22:50:30 -0500

    added insert proj

--- routes/project/index.js
+++ routes/project/index.js
@@ -13,10 +13,29 @@ function handleProjects(req, reply){
   })
 }
 /****-- end handleProjects --***/
+
+/****** handleInsertPrj ********/
+function handleInsertPrj(req, reply){
+  (async function(){
+    const {proj_name, due_date, state, description} = req.payload;
+    var results=await db.execute(sql.project.insert, [proj_name, due_date, state, description]);
+    reply();
+  })()
+  .catch((err)=>{
+    throw err;
+  })
+}
+/*****-- end handleInsertPrj ---*****/
+
 module.exports=[
   {
     method:'GET',
     path:'/projects',
     handler:handleProjects
+  },
+  {
+    method:'POST',
+    path:'/insert/project',
+    handler:handleInsertPrj
   }
 ]
